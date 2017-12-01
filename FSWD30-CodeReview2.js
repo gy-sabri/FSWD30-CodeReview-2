6f4528568b034c12e2ad7784808e3fdb45823e85
Moaz asabri <gy.sabri@hotmail.com>
2017-12-02 00:55:32 +0100

    New Update

--- FSWD30-CodeReview2.js
+++ FSWD30-CodeReview2.js
@@ -1,26 +1,30 @@
 function myFun() {
     
-    amsterdam = [1500, 500, 1800, 700],
     london = [2500, 1500, 1000, 900],
+    amsterdam = [1500, 500, 1800, 700],
+       
+    l = 0,
+    a = 0,
 
-    amsterdamC = amsterdam[0] + amsterdam[1] + amsterdam[2] + amsterdam[3],
-
-    londonC = london[0] + london[1] + london[2] + london[3],
-
-    amsterdamX = amsterdamC / 4,
-
-    londonX = londonC / 4;
+    londonC = london.length,
+    amsterdamC = amsterdam.length,
+    
+    Nul = 0,
+    Nua = 0;
+     
+    for (; l < londonC; ) { Nul += london[l]; l++;};
+    for (; a < amsterdamC; ) { Nua += amsterdam[a]; a++;};
+    
+    londonX = Nul / london.length;  
+    amsterdamX = Nua / amsterdam.length,
         
     document.getElementById("ams").innerHTML = amsterdamX ;
     document.getElementById("lon").innerHTML = londonX ;
     
-
     if(amsterdamX > londonX) {
         document.getElementById("Pr").innerHTML = amsterdamX - londonX ;
         document.getElementById("Text").innerHTML = "<span>Amsterdam </span>Will be more affordable than <span>London </span>for the New Year's celebration Difference in price" ;
         
-
-
     } else {
 
         document.getElementById("Pr").innerHTML = londonX - amsterdamX ;
